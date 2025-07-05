6e07812a82c9a00422b87125df20dd44e5e4fca2
bhawana-d <mailtogcd@yahoo.com>
2025-07-05 18:26:07 +0530

    Create CanArrange.java

---
+++ LC/src/HashSetORMap/CanArrange.java
@@ -0,0 +1,25 @@
+
+//https://leetcode.com/problems/check-if-array-pairs-are-divisible-by-k/description/
+//TC = O(n)
+//SC = O(K)
+class Solution {
+    public boolean canArrange(int[] arr, int k) {
+        int[] mp = new int[k];
+
+        for(int num : arr){
+            int rem = (num%k + k)%k;
+            mp[rem]++;
+        }
+        if(mp[0] %2 != 0){
+            return false;
+        }
+
+        for(int rem = 1;rem <= k/2;rem++){
+            int counterHalf = k - rem;
+            if(mp[counterHalf] != mp[rem]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
