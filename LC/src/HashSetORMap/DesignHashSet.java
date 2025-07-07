b2a0035ea6190f7ebc690804c0928ae9836389e6
bhawana-d <mailtogcd@yahoo.com>
2025-07-07 22:30:20 +0530

    Create DesignHashSet.java

---
+++ LC/src/HashSetORMap/DesignHashSet.java
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/design-hashset/description/
+class MyHashSet {
+
+    private boolean[] arr ;
+    public MyHashSet() {
+        arr = new boolean[10_000_01];
+    }
+    
+    public void add(int key) {
+        arr[key] = true;
+    }
+    
+    public void remove(int key) {
+        arr[key] = false;
+    }
+    
+    public boolean contains(int key) {
+        return arr[key];
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
