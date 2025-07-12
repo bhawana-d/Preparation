bf29543ac78be1eb62ced23cb8f2bbd476987b25
bhawana-d <mailtogcd@yahoo.com>
2025-07-12 12:15:32 +0530

    Update CountBadPairs.java

--- LC/src/HashSetORMap/CountBadPairs.java
+++ LC/src/HashSetORMap/CountBadPairs.java
@@ -24,3 +24,19 @@ class Solution {
         return res;
     }
 }
+///APPROACH 2
+ int n = nums.length;
+        long result = 0;
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        
+        for (int i = 0; i < n; i++) {
+            int diff = nums[i] - i;
+            int totalPairsTillIndex = i;
+            int goodPairs = mp.getOrDefault(diff, 0);
+            
+            result += (totalPairsTillIndex - goodPairs);
+            
+            mp.put(diff, goodPairs + 1);
+        }
+        
+        return result;
