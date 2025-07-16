09eaf6f38e2efe72cc6fa14567a101420b7eedf2
bhawana-d <mailtogcd@yahoo.com>
2025-07-16 23:51:04 +0530

    Update RabbitsInForest.java

--- LC/src/HashSetORMap/RabbitsInForest.java
+++ LC/src/HashSetORMap/RabbitsInForest.java
@@ -16,3 +16,21 @@ class Solution {
         return ans;
     }
 }
+//----------
+    class Solution {
+    public int numRabbits(int[] answers) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int ans =0;
+        int n = answers.length;
+        for(int i = 0;i < n;i++){
+            map.put(answers[i] , map.getOrDefault(answers[i]  , 0) + 1);
+        }
+        for(int i : map.keySet()){
+            int grpSize = i + 1;
+            int grp = (int)Math.ceil((double)map.get(i) / grpSize);
+
+            ans += grp * grpSize;
+        }
+        return ans;
+    }
+}
