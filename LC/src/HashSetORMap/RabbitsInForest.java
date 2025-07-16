ec894c5e6c384946baa053a4b8922f090c5a1ddb
bhawana-d <mailtogcd@yahoo.com>
2025-07-16 23:40:30 +0530

    Create RabbitsInForest.java

---
+++ LC/src/HashSetORMap/RabbitsInForest.java
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/rabbits-in-forest/description/
+class Solution {
+    public int numRabbits(int[] answers) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int ans =0;
+        int n = answers.length;
+        for(int i = 0;i < n;i++){
+            map.put(answers[i] , map.getOrDefault(answers[i]  , 0) + 1);
+        }
+        for(int i : map.keySet()){
+            int grpSize = i + 1;
+            int grp = (i + map.get(i)) / grpSize;
+
+            ans += grp * grpSize;
+        }
+        return ans;
+    }
+}
