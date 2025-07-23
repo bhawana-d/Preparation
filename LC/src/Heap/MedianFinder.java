5b78187a5b9803c0cc8182426102e1d2296c9f96
bhawana-d <mailtogcd@yahoo.com>
2025-07-24 01:30:58 +0530

    Update MedianFinder.java

--- LC/src/Heap/MedianFinder.java
+++ LC/src/Heap/MedianFinder.java
@@ -1,4 +1,6 @@
 //https://leetcode.com/problems/find-median-from-data-stream/description/
+//SC= O(50001) ~= O(1)
+//TC = O(n^2)
 class MedianFinder {
     private int[] vec;
     private int i;
@@ -29,6 +31,47 @@ class MedianFinder {
             return (double)(vec[l] + vec[r])/2;
         }
         return (double)(vec[n/2]);
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
+//SC= O(N)
+//TC = O(log N) for add num O(1) for findmedian
+
+class MedianFinder {
+    private PriorityQueue<Integer> leftMaxHeap;
+    private PriorityQueue<Integer> rightMinHeap;
+    public MedianFinder() {
+        leftMaxHeap = new PriorityQueue<>(Collections.reverseOrder()); //MaxHeap
+        rightMinHeap = new PriorityQueue<>();
+    }
+    
+    public void addNum(int num) {
+        if(leftMaxHeap.isEmpty() || leftMaxHeap.peek() > num){
+            leftMaxHeap.add(num);
+        }else{
+            rightMinHeap.add(num);
+        }
+
+        //always maintain size + 1 in right min heap
+        if(Math.abs(leftMaxHeap.size() - rightMinHeap.size()) > 1){
+            rightMinHeap.add(leftMaxHeap.poll());
+        }else if(rightMinHeap.size() > leftMaxHeap.size()){
+            leftMaxHeap.add(rightMinHeap.poll());
+        }
+    }
+    
+    public double findMedian() {
+        if(leftMaxHeap.size() == rightMinHeap.size()){
+            return (double)(leftMaxHeap.peek() + rightMinHeap.peek())/2;
+        }else{
+            return leftMaxHeap.peek();
+        }
     }
 }
 
