23a314239322926dcfb702528b4f9679bba8e3f2
bhawana-d <mailtogcd@yahoo.com>
2025-08-11 21:22:58 +0530

    Update FindkPairsWithSmallestSums.java

--- LC/src/Heap/FindkPairsWithSmallestSums.java
+++ LC/src/Heap/FindkPairsWithSmallestSums.java
@@ -33,3 +33,41 @@ class Solution {
         return res;
     }
 }
+//--------------------------------- slightly optimised solution
+class Solution {
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        int m = nums1.length;
+        int n = nums2.length;
+
+        Set<String> visited = new HashSet<>();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0] - b[0]);
+        List<List<Integer>> res = new ArrayList<>();
+
+        pq.offer(new int[]{nums1[0] + nums2[0],0,0});
+        visited.add("0,0");
+        while(k-- > 0 && !pq.isEmpty()){
+            int[] temp = pq.poll();
+            int i = temp[1];
+            int j = temp[2];
+            res.add(List.of(nums1[i],nums2[j]));
+
+            if(j+1 < n ){
+                String nextPairKey = i + "," + (j+1);
+                if(!visited.contains(nextPairKey)){
+                    pq.offer(new int[]{nums1[i] + nums2[j+1],i,j+1});
+                    visited.add(nextPairKey);
+                }
+            }
+
+            if(i+1 < m ){
+                String nextPairKey = (i + 1) + "," + j;
+                if(!visited.contains(nextPairKey)){
+                    pq.offer(new int[]{nums1[i+1] + nums2[j],i+1,j});
+                    visited.add(nextPairKey);
+                }
+            }
+        }
+
+    return res;
+    }
+}
